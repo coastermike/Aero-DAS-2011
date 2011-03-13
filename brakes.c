00ca7f6749b4bf1de03b6e189db5b01a4b895a4d
Michael Seppanen <coastermike@gmail.com>
2011-03-13 03:58:06 -0400

    Hopefully have input PWM capture working with work around using INT2

--- brakes.c
+++ brakes.c
@@ -7,17 +7,19 @@
 #include "adc_read.h"
 
 void toggle_LED1 (void);
-void toggle_BrakeL (void);
+void toggle_LED2 (void);
 void toggle_BrakeR (void);
 void hall_L (void);
 void hall_R (void);
+void PWMRead(void);
+void Timer3Reset(void);
 void HIGH_ISR(void);
 
 const unsigned float constant = .6;
 unsigned int TimeIntClk = 0, HallCountL = 0, HallCountR = 0, TempHallCountL = 0, TempHallCountR = 0, risetime = 0, falltime = 0;
 unsigned char overflowCount = 0, leftForceRead = 0, rightForceRead = 0;
-unsigned char history = 0, history1 = 0, save = 0;
-unsigned int noLoadL = 0, noLoadR = 0, LoadL = 0, LoadR = 0, pulse = 0;
+unsigned char history = 0, history1 = 0, save = 0, pwmRiseFall = 0, timer3H = 0, timer3L = 0;
+unsigned int noLoadL = 0, noLoadR = 0, LoadL = 0, LoadR = 0, pulse = 0, timer3 = 0;
 
 #pragma code HIGH_INTERRUPT_VECTOR = 0x8	//Where the code goes when a high priority interrupt happens
 void high_interrupt (void)
@@ -104,9 +106,45 @@ void hall_R(void)
 	INTCON3bits.INT1IF = 0;
 }
 
-#pragma interrupt measure_PWM
-void measure_PWM(void)
+#pragma interrupt PWMRead
+void PWMRead(void)
 {
+	if(pwmRiseFall == 0)
+	{
+		INTCON2bits.INTEDG2 = 0;	//falling
+		INTCON3bits.INT2IF = 0;
+		TMR3H = 0x00;				//sets initial as 0
+		TMR3L = 0x00;
+		T3CONbits.TMR3ON = 1;
+		pwmRiseFall = 1;
+	}
+	else if(pwmRiseFall == 1)
+	{
+		INTCON2bits.INTEDG2 = 1;	//rising
+		T3CONbits.TMR3ON = 0;
+		timer3 = ((int)timer3H << 8 ) | timer3L;
+		INTCON3bits.INT2IF = 0;
+		pwmRiseFall = 0;
+		if(timer3 < 1)
+		{
+			Set_Speed(0);
+		}
+		else
+		{
+			Set_Speed(timer3);
+		}	
+	}	
+}
+	
+#pragma interrupt Timer3Reset
+void Timer3Reset(void)
+{
+	PIR2bits.TMR3IF = 0;
+}
+	
+//#pragma interrupt measure_PWM
+//void measure_PWM(void)
+//{
 //	if(CCP1CONbits.CCP1M0 == 1)	//rising
 //	{
 //		risetime = CCPR1H;
@@ -129,21 +167,21 @@ void measure_PWM(void)
 //		TMR3H = 0x00;		//reset timer to 0
 //		TMR3L = 0x00;
 //		pulse = falltime-risetime;
-	
-		pulse = ReadCapture1();
-		PIR1bits.CCP1IF = 0;
-		PIR2bits.TMR3IF = 0;
-		if(pulse < 2100)
-		{
-			Set_Speed(0);
-		}
-		else
-		{
-			Set_Speed(pulse);
-		}	
-//Insert code for setting timer1 for the brake here
-	
-}
+//	
+//		pulse = ReadCapture1();
+//		PIR1bits.CCP1IF = 0;
+//		PIR2bits.TMR3IF = 0;
+//		if(pulse < 2100)
+//		{
+//			Set_Speed(0);
+//		}
+//		else
+//		{
+//			Set_Speed(pulse);
+//		}	
+////Insert code for setting timer1 for the brake here
+//	
+//}
 
 void HIGH_ISR (void)				//Figures out what interrupt triggered and runs the code.
 {
@@ -159,10 +197,22 @@ void HIGH_ISR (void)				//Figures out what interrupt triggered and runs the code
 		goto hall_R
 		_endasm
 	}
-	else if (PIR1bits.CCP1IF && PIE1bits.CCP1IE)
+//	else if (PIR1bits.CCP1IF && PIE1bits.CCP1IE)
+//	{
+//		_asm
+//		goto measure_PWM
+//		_endasm
+//	}
+	else if (INTCON3bits.INT2IF && INTCON3bits.INT2IE)
 	{
 		_asm
-		goto measure_PWM
+		goto PWMRead
+		_endasm
+	}	
+	else if (PIR2bits.TMR3IF && PIE2bits.TMR3IE)
+	{
+		_asm
+		goto Timer3Reset
 		_endasm
 	}	
 	else if(PIR1bits.TMR1IF && PIE1bits.TMR1IE)
@@ -186,9 +236,11 @@ void Brakes_Init(void)
 	INTCON2bits.TMR0IP = 1;		//timer0 high priority
 	IPR2bits.TMR3IP = 1;		//timer3 high priority
 	INTCON3bits.INT1IP = 1;		//INT1 high priority (INT0 is always high)
-	IPR1bits.CCP1IP = 1;		//CCP1 high priority
+	INTCON3bits.INT2IP = 1;		//INT2 high priority
+//	IPR1bits.CCP1IP = 1;		//CCP1 high priority
 	INTCON2bits.INTEDG0 = 0;	//enables INT0 on falling edge
 	INTCON2bits.INTEDG1 = 0;	//enables INT1 on falling edge
+	INTCON2bits.INTEDG2 = 1;	//enables INT2 on rising edge
 	INTCONbits.GIEH = 1;		//enables high priority interrupts
 	INTCONbits.GIEL = 1;		//enables low priority interrupts
 	PIE1bits.TMR1IE = 1;		//enable timer1 interrupt
@@ -196,19 +248,23 @@ void Brakes_Init(void)
 	PIE2bits.TMR3IE = 1;		//enable timer3 interrupt
 	INTCONbits.INT0IE = 1;		//enable INT0 interrupt
 	INTCON3bits.INT1IE = 1;		//enable INT1 interrupt
+	INTCON3bits.INT2IE = 1;		//enables INT2 interrupt
 //	CCP1CON = 0b00000101;		//sets up CCP1 as rising edge capture
 //	PIE1bits.CCP1IE = 1;		//enable CCP1 interrupt -- elsewhere?
-	OpenCapture1(CAPTURE_INT_ON & CAP_EVERY_FALL_EDGE);
+//	OpenCapture1(CAPTURE_INT_ON & CAP_EVERY_RISE_EDGE);
 	T0CON = 0b00000111;			//sets up timer 0, 1:256 prescalar
 	TMR0H = 0xE1;				//sets initial as 0
 	TMR0L = 0x7B;				//
 	T1CON = 0b11110000;			//sets up timer 1, 1:8 for LED status
 	TMR1H=0x0B;					//sets initial as 0
 	TMR1L=0xF7;					//
-	T3CON = 0b11100000;			//sets up timer3, 1:1 FOR CCP1
+//	T3CON = 0b11110000;			//sets up timer3, 1:8 FOR CCP1
+	T3CONbits.RD16 = 1;
+	T3CONbits.T3CKPS1 = 0b11;
+	T3CONbits.TMR3CS = 0;
 	TMR3H = 0x00;				//sets initial as 0
 	TMR3L = 0x00;				//
-	T1CONbits.TMR1ON = 1;		//Turns timer2 on for the LED status
+	T1CONbits.TMR1ON = 1;		//Turns timer1 on for the LED status
 //	T3CONbits.TMR3ON = 1;
 }
 
@@ -248,7 +304,7 @@ unsigned float get_Hall_R(void)
 //waits for SW1 or 2 to be pressed for >1sec, then stores the values while unloaded.
 void calibrateNoLoad(void)
 {
-	if((SW1 || SW2) && history == 0)
+	if((SW2) && history == 0)
 	{
 		TMR0H = 0xE1;
 		TMR0L = 0x7B;
@@ -257,13 +313,13 @@ void calibrateNoLoad(void)
 	}
 	if(history == 2)
 	{
-		while(SW1 || SW2){}
+		while(SW2){}
 		noLoadL = Adc_Read(0);
 		noLoadR = Adc_Read(1);
 		LED6 = ~LED6;
 		history = 0;
 	}
-	if(!SW1 && !SW2 && history == 1)
+	if(!SW2 && history == 1)
 	{
 		history = 0;
 		T0CONbits.TMR0ON = 0;
